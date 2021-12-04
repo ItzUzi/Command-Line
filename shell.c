452dda366de7f8e6eac0cb8b7f049d924bf084a0
Uziel Gaeta <ugaeta@cpp.edu>
2021-12-03 22:37:01 -0800

    Finished builtin methods functions, moved around program to follow tutorial better

--- shell.c
+++ shell.c
@@ -1,32 +1,92 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void lsh_loop(void);
 char *lsh_read_line(void);
 char **lsh_split_line(char *line);
 
-int main(int argc, char **argv){
+/**
+ * Function declation for builtin shell commands 
+ */
 
-    lsh_loop();
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
 
-    return EXIT_SUCCESS;
-}
-
-void lsh_loop(void){
-
-    char *line;
-    char **args;
+int lsh_launch(char **args){
+    pid_t pid, wpid;
     int status;
 
-    do{
-        printf("> ");
-        line = lsh_read_line();
-        args = lsh_split_line(line);
-        status = lsh_execute(args);
+    pid = fork();
+    if(pid ==0){
+        //Child process
+        if(execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if(pid<0){
+        // Error Forking
+        perror("lsh");
+    } else {
+        //Parent process
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
 
-        free(line);
-        free(args);
-    }while(status);
+    return 1;
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line){
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if(!tokens){
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while(token != NULL){
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if(!tokens){
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL,LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+char *lsh_read_line(void){
+    char *line = NULL;
+    ssize_t bufsize = 0; // have getline allocate a buffer
+
+    if(getline(&line, &bufsize, stdin) == -1){
+        if(feof(stdin)) {
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
 }
 
 #define LSH_RL_BUFSIZE 1024
@@ -66,50 +126,29 @@ char *lsh_read_line(void){
     }
 }
 
-char *lsh_read_line(void){
-    char *line = NULL;
-    ssize_t bufsize = 0; // have getline allocate a buffer
+void lsh_loop(void){
 
-    if(getline(&line, &bufsize, stdin) == -1){
-        if(feof(stdin)) {
-            exit(EXIT_SUCCESS);
-        } else {
-            perror("readline");
-            exit(EXIT_FAILURE);
-        }
-    }
+    char *line;
+    char **args;
+    int status;
 
-    return line;
+    do{
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    }while(status);
 }
 
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
-char **lsh_split_line(char *line){
-    int bufsize = LSH_TOK_BUFSIZE, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
+int main(int argc, char **argv){
 
-    if(!tokens){
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+    lsh_loop();
 
-    token = strtok(line, LSH_TOK_DELIM);
-    while(token != NULL){
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize) {
-            bufsize += LSH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if(!tokens){
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL,LSH_TOK_DELIM);
-    }
-    tokens[position] = NULL;
-    return tokens;
+    return EXIT_SUCCESS;
 }
+
+
+
