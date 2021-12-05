9c1fc928850daceb3be15a86f50606c6e1036e12
Uziel Gaeta <ugaeta@costello.unx.cpp.edu>
2021-12-04 16:23:29 -0800

    Fixed typo

--- kilo.c
+++ kilo.c
@@ -32,7 +32,7 @@ void enableRawMode(){
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    raw.c_cc[VNIM] = 0;
+    raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
